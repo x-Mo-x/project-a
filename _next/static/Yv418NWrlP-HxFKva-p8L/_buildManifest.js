7828ff046b32f3e974eeec4efbd341aa8a75f699
x-Mo-x <111273700+x-Mo-x@users.noreply.github.com>
2022-08-15 17:25:19 +0800

    Update

--- _next/static/Yv418NWrlP-HxFKva-p8L/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-fbcfe2172188e46f.js","static/chunks/897-8a6582c50e6a7e4b.js","static/chunks/pages/index-024c94fb85e55ada.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
